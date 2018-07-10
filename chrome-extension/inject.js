2ac738a4dae6626548328758f8c4ce14c71c0795
domainio <erezhr@tikalk.com>
2018-07-10 15:29:27 +0300

    fix video tag

--- chrome-extension/inject.js
+++ chrome-extension/inject.js
@@ -13,9 +13,21 @@
 
   // alert('inserted self... giggity');
 
+  var url = chrome.runtime.getURL('data/my-model-1.json');
+
+  fetch(url)
+  .then((response) => response.json()) //assuming file contains json
+  .then((json) => {
+    console.log('json fetched: ', json);
+  });
+
+
   if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     // Not adding `{ audio: true }` since we only want video now
     navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+      if (!video) {
+        video = document.getElementById('video');
+      }
       video.src = window.URL.createObjectURL(stream);
       video.play();
       console.log('playing')
