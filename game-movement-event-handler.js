f87fe150c76d92b245d0fe3b8f177622933614a3
Alexs <sawenok@gmail.com>
2018-07-10 22:56:45 +0300

    stop on center face position

--- game-movement-event-handler.js
+++ game-movement-event-handler.js
@@ -9,47 +9,48 @@
     let prevPos = ''
     window.addEventListener('facePosition', (evt) => {
         switch (evt.detail.position) {
+            
             case positions.down: {
                 
-                if (prevPos !== positions.down) {
+                // if (prevPos !== positions.down) {
                     console.log('down')
                     movedown()
                     prevPos = positions.down;
-                }
+                // }
                 break;
             }
             case positions.up: {
                 
-                if (prevPos !== positions.up) {
+                // if (prevPos !== positions.up) {
                     console.log('up')
                     moveup()
                     prevPos = positions.up;
-                }
+                // }
                 break;
             }
             case positions.left: {
                 
-                if (prevPos !== positions.left) {
+                // if (prevPos !== positions.left) {
                     console.log('left')
                     moveleft()
                     prevPos = positions.left;
-                }
+                // }
                 break;
             }
             case positions.right: {
                 
-                if (prevPos !== positions.right) {
+                // if (prevPos !== positions.right) {
                     console.log('right')
                     moveright()
                     prevPos = positions.right;
-                }
+                // }
                 break;
             }
-            // case positions.center: {
-            //     console.log('center')
-            //     clearmove()
-            //     break;
-            // }
+            default: {
+                clearmove()
+                console.log('stop')
+                break;
+            }
 
         }
     })
