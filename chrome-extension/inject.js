c081d33f6aa70d9d24d74279786dc05e1e02fb58
domainio <erezhr@tikalk.com>
2018-07-10 14:37:32 +0300

    video tag

--- chrome-extension/inject.js
+++ chrome-extension/inject.js
@@ -3,20 +3,21 @@
 (function() {
 
   // just place a div at top right
-  var div = document.createElement('div');
-  div.style.position = 'fixed';
-  div.style.top = 0;
-  div.style.right = 0;
-  div.textContent = 'Injected!';
-  document.body.appendChild(div);
+  // var div = document.createElement('div');
+  // div.style.position = 'fixed';
+  // div.style.top = 0;
+  // div.style.right = 0;
+  // div.textContent = 'Injected!';
+  // document.body.appendChild(div);
+  var video = document.getElementById('video');
 
-  alert('inserted self... giggity');
+  // alert('inserted self... giggity');
 
   if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     // Not adding `{ audio: true }` since we only want video now
     navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-      // video.src = window.URL.createObjectURL(stream);
-      // video.play();
+      video.src = window.URL.createObjectURL(stream);
+      video.play();
       console.log('playing')
     });
   }
